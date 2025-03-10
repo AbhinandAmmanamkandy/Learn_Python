cd57bd14fdc639c0ea60cdbb306a109577ce8320
AbhinandAmmanamkandy <abhinandammanamkandypvt@gmail.com>
2025-03-10 23:41:19 +0530

    first commit

---
+++ 1. Variables/variables.py
@@ -0,0 +1,24 @@
+# Strings
+first_name = "Abhinand"
+food = "pizza"
+email = "abhinandammanamkandy@gmail.com"
+
+#Integers
+age = 25
+quantity = 3
+num_of_students = 30
+
+# Float
+price = 10.99
+gpa = 3.2
+distance = 5.5
+
+# Boolean
+is_student = False
+for_sale = False
+is_online = True
+
+if is_online:
+    print("You are online")
+else:
+    print("You are offline")
