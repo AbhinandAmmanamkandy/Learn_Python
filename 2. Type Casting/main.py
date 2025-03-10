2d4efa2a8889f632c0aa40fb3cd3a8023d09c7cd
AbhinandAmmanamkandy <abhinandammanamkandypvt@gmail.com>
2025-03-10 23:48:15 +0530

    Type casting

---
+++ 2. Type Casting/main.py
@@ -0,0 +1,16 @@
+#  str(), int(), float(), bool()
+
+name = "Abhinand Ammanamkandy"
+age = 25
+gpa = 3.2
+is_student = True
+
+# Return True until and unless the string is empty
+name = bool(name)
+print(name)
+
+age = float(age)
+print(age)
+
+gpa = int(gpa)
+print(gpa)
