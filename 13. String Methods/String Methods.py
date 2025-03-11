dde0c922355fc9062493ab2be0a078c381fcfee2
AbhinandAmmanamkandy <abhinandammanamkandypvt@gmail.com>
2025-03-11 08:22:41 +0530

    String Methods

---
+++ 13. String Methods/String Methods.py
@@ -0,0 +1,19 @@
+# name = input("Enter your full name: ")
+
+# phone_number = input("Enter your phone number: ")
+
+# res = len(name) # Length of string
+# result = name.find("B") # Find index of letter B
+# result = name.rfind("A") # Reverse find
+# name = name.capitalize() # First letter capitilize
+# name = name.upper() # All letters upper case
+# name = name.lower() # All letters lower case
+# result = name.isdigit() # Can only contain digits if so True else False
+# result = name.isalpha() # Can only contains alphabets, no spaces or numbers
+
+# result = phone_number.count("-") # COunt characters
+# result = phone_number.replace("-", "")
+
+# print(result)
+
+print(help(str.capitalize))
