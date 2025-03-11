214f4d6d52e378be8cc1965e94b57785759eca20
AbhinandAmmanamkandy <abhinandammanamkandypvt@gmail.com>
2025-03-11 09:30:16 +0530

    For Loop

---
+++ 18. For Loop/For Loop.py
@@ -0,0 +1,32 @@
+# For loop = execute a block of code a fixed number of times.
+#           You can iterate over a range, string, sequence etc
+
+# reversed - reverses counting
+
+# for x in range(1, 11):
+#     print(x)
+
+# for x in reversed(range(1, 11)):
+#     print(x)
+
+# print("HAPPY NEW YEAR")
+
+# for x in range(1, 11, 2): Here 2 is steps
+#     print(x)
+
+# credit_card = "1234-5678-9012-3456"
+
+# for x in credit_card:
+#     print(x)
+
+# for x in range(1, 21):
+#     if x == 13:
+#         continue
+#     else:
+#         print(x)
+
+# for x in range(1, 21):
+#     if x == 13:
+#         break
+#     else:
+#         print(x)
