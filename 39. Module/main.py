44dc72ee32e894192ecdbae8de826a94531684b4
MuzicReborn <muzicreborn@gmail.com>
2025-03-12 10:48:45 +0530

    Modules

---
+++ 39. Module/main.py
@@ -0,0 +1,21 @@
+# Module - a file containing code you want to include in your program
+#       use 'import' to include a module (built-in on your own)
+#       useful to break up a large program reusable seperate files
+
+# Here m is alias
+# import math as m
+
+# print(m.pi)
+
+# from math import e
+
+# print(e)
+
+# Custom module
+import create
+
+result = create.pi
+result = create.square(3)
+result = create.cube(3)
+result = create.circumference(3)
+result = create.area(3)
