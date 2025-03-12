8bfd3b48fa05f4dc0ef419aaa35dee3175dccd5b
MuzicReborn <muzicreborn@gmail.com>
2025-03-12 09:32:44 +0530

    Iterables

---
+++ 35. Iterables/iterables.py
@@ -0,0 +1,27 @@
+# Iterables = An object/collection that can return its elements one at a time,
+#           allowing it to be iterated over a loop
+
+# numbers = (1, 2, 3, 4, 5)
+
+# for number in numbers:
+#     print(number, end=" ")
+
+# fruits = {"apple", "orange", "banana", "coconut"}
+
+# for fruit in fruits:
+#     print(fruit)
+
+# name = "Ghost Reborn"
+
+# for character in name: 
+#     print(character, end=" ")
+
+#Dictionary
+# my_dictionary = {
+#     "A": 1,
+#     "B": 2,
+#     "C": 3
+# }
+
+# for key, value in my_dictionary.items():
+#     print(f"{key} = {value}")
