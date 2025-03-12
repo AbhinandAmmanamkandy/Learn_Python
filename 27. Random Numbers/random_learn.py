58eb725f37d3e955ae642851587109a83adc570a
MuzicReborn <muzicreborn@gmail.com>
2025-03-12 06:42:08 +0530

    Random Numbers

---
+++ 27. Random Numbers/random_learn.py
@@ -0,0 +1,14 @@
+import random
+
+low = 1
+high = 100
+options = ("rock", "paper", "scissors")
+cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+
+
+# num = random.randint(low, high)
+# num = random.random()
+# option = random.choice(options) # randomized choice
+# random.shuffle(cards) # Shuffle the list
+
+print(cards)
