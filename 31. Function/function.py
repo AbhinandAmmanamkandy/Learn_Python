0a212cfebd0ba3593f064b915f68667893569f94
MuzicReborn <muzicreborn@gmail.com>
2025-03-12 07:46:11 +0530

    Functions

---
+++ 31. Function/function.py
@@ -0,0 +1,10 @@
+# Function = A block of reusable code
+#           place () after the function to invoke it
+
+def happy_birthday(name, age):
+    print(f"Happy Birthday {name}")
+    print(f"You are {age} yrs old!")
+    print()
+
+happy_birthday("Abhinand", 23)
+happy_birthday("Steve", 19)
---
+++ 31. Function/three.py
@@ -0,0 +1,19 @@
+# return = statement used to end a function
+#       and send a result back to the caller
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
+print(add(1, 2))
+print(sub(1, 2))
+print(mul(1, 2))
+print(div(1, 2))
