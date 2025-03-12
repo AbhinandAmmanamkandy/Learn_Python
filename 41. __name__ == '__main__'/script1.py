78589a32c65a1cff6472d5d2a5ea6ea78089e355
MuzicReborn <muzicreborn@gmail.com>
2025-03-12 11:20:05 +0530

    name == main

---
+++ 41. __name__ == '__main__'/script1.py
@@ -0,0 +1,10 @@
+def favorite_food(food):
+    print(f"Your favorite food is {food}")
+
+def main():
+    print("This is script 1")
+    favorite_food("Pizza")
+    print("Good Bye!")
+
+if __name__ == '__main__':
+    main()
