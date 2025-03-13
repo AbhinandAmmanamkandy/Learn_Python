277029b81c8698bfa6c4073cb4aab92b51eadf94
MuzicReborn <muzicreborn@gmail.com>
2025-03-13 06:19:50 +0530

    Polymorphism

---
+++ 51. Polymorphism/main.py
@@ -0,0 +1,48 @@
+# Polymorphism = Greek word that means to "have mamny forms or faces"
+#       Poly = many
+#       Morphe = Form
+
+#       Two ways to achieve polymorphism
+#       1. Inheritance = an object could be treated of same type as a parent class
+#       2. "Duck Typng" = Object must have necessary attributes/methods
+
+from abc import ABC, abstractmethod
+
+class Shape:
+
+    @abstractmethod
+    def area(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius ** 2
+
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return self.side ** 2
+
+class Triangle(Shape):
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+    def area(self):
+        return self.base * self.height * 0.5
+    
+class Pizza(Circle):
+    def __init__(self, topping, radius):
+        self.topping = topping
+        super().__init__(radius)
+
+shapes = [Circle(5), Square(5), Triangle(5,6), Pizza("Pepperoni", 15)]
+
+
+for shape in shapes:
+    print(f"{shape.area()}cm²")
