c5d7ff857cf1f96a390ab509c80f84f02c9d5ab9
MuzicReborn <muzicreborn@gmail.com>
2025-03-13 11:39:11 +0530

    Fix Folders

--- 66. PyQt5 Intro/main.py
+++ 66. PyQt5 Intro/main.py
@@ -1,27 +1,13 @@
 # PyQt5 Introduction
 
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
-from PyQt5.QtGui import QIcon
-from PyQt5.QtGui import QFont
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("My Cool First GUI")
         self.setGeometry(700, 300, 500, 500)
-        self.setWindowIcon(QIcon("dp.jpg"))
-
-        label = QLabel("Hello", self)
-        label.setFont(QFont("Arial", 30))
-        label.setGeometry(0, 0, 500, 100)
-        label.setStyleSheet("color: #292929;"
-                            "background-color: #6fdcf7;"
-                            "font-weight: bold;"
-                            "font-style: italic;"
-                            "text-decoration: underline;")
-        label.setAlignment(Qt.AlignCenter)
     
 def main():
     app = QApplication(sys.argv)
